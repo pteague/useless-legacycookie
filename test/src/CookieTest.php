1259ab2923b11bf98f38d224e33987453751bba4
Patrick Teague <patrick@whitelionsoft.com>
2015-01-04 14:15:20 -0600

    added a couple of tests for CookieTest

--- test/src/CookieTest.php
+++ test/src/CookieTest.php
@@ -24,8 +24,19 @@ namespace Useless\Legacy;
 class CookieTest
 	extends \PHPUnit_Framework_TestCase
 {
+	/**
+	 * @var \Useless\Legacy\Cookie
+	 */
 	protected $cookie;
+
+	/**
+	 * @var \Useless\Legacy\SimpleHashMapInterface
+	 */
 	protected $cookieMap;
+
+	/**
+	 * @var \Useless\Legacy\SimpleHashMapInterface
+	 */
 	protected $sessionMap;
 
 	public function setUp()
@@ -67,8 +78,74 @@ class CookieTest
 		$this->assertEquals( $cookieMap, $cookie->getCookieObject() );
 	}
 
-	public function testCookie()
+	public function providerTestCookie()
 	{
-		$this->assertTrue( true );
+		return array(
+			array( array(
+				'name' => 'foo',
+				'value' => null,
+				'expire' => 0,
+				'path' => null,
+				'domain' => null,
+				'secure' => false,
+				'httponly' => false,
+			), ),
+			array( array(
+				'name' => 'foo',
+				'value' => 'bar',
+				'expire' => 60,
+				'path' => '/',
+				'domain' => null,
+				'secure' => true,
+				'httponly' => false,
+			), ),
+			array( array(
+				'name' => 'bar',
+				'value' => 'baz',
+				'expire' => 360,
+				'path' => null,
+				'domain' => null,
+				'secure' => false,
+				'httponly' => false,
+			), ),
+			array( array(
+				'name' => 'baz',
+				'value' => 'foo',
+				'expire' => 0,
+				'path' => '/',
+				'domain' => null,
+				'secure' => false,
+				'httponly' => false,
+			), ),
+		);
 	}
+
+	/**
+	 * @dataProvider providerTestCookie
+	 * @param $cookie
+	 */
+	public function testCookieUseLegacy( $cookie )
+	{
+		$this->cookie->setUseLegacyCookies( true );
+		call_user_func_array( array( $this->cookie, 'setCookie' ), $cookie );
+
+		$this->assertEquals( $cookie['value'], $this->sessionMap->get( $cookie['name'] ) );
+		$cookieMap = FakeCookie::getCookies();
+		$this->assertEquals( $cookie, $cookieMap->get( $cookie['name'] ) );
+	}
+
+	/**
+	 * @dataProvider providerTestCookie
+	 * @param $cookie
+	 */
+	public function testCookieNoLegacy( $cookie )
+	{
+		$this->cookie->setUseLegacyCookies( false );
+		call_user_func_array( array( $this->cookie, 'setCookie' ), $cookie );
+
+		$this->assertEquals( $cookie['value'], $this->sessionMap->get( $cookie['name'] ) );
+		$cookieMap = FakeCookie::getCookies();
+		$this->assertNull( $cookieMap->get( $cookie['name'] ) );
+	}
+
 }
